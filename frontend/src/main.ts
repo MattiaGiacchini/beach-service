316962fc57c4ae52e07178642108a13a7a8bd2b1
Mattia <giacchini.mattia@gmail.com>
2025-03-17 21:55:58 +0100

    Update versions

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,45 @@
 import './assets/main.scss'
-import 'primevue/resources/themes/lara-dark-blue/theme.css'
 import FocusTrap from 'primevue/focustrap'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes'
+
+const AuraPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}'
+    }
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: AuraPreset,
+    options: {
+      prefix: 'bs',
+      darkModeSelector: 'system',
+      cssLayer: false
+    }
+  }
+})
 app.directive('focustrap', FocusTrap)
 
-
 app.mount('#app')
-
-
-
-
